4307b239702061c04635d3da97ab2cd33ce2725e
Hutlaw <154370020+Hutlaw@users.noreply.github.com>
2024-02-16 22:59:14 -0500

    Delete drag.js

--- drag.js
+++
@@ -1,6 +0,0 @@
-// Event listeners for mousedown on elements
-document.querySelectorAll('.element').forEach(element => {
-    element.addEventListener('mousedown', (event) => {
-        event.dataTransfer.setData('text/plain', element.textContent);
-    });
-});
