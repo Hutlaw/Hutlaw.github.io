d88b4e1e26ccbf9339e63c12e50f04cb2e07f333
Hutlaw <154370020+Hutlaw@users.noreply.github.com>
2024-06-11 19:39:51 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,12 +1,9 @@
-// Settings, used for easy customization for me. why did i make this? shut up, dont ask questions
-// 1 = enabled, 0 = disabled
+// Settings
 const showProfilePicture = 1; // Change this value to 0 to hide the profile picture
-// End of settings, everything else belows is to edit the scripts based on input
-
-
-// Function to toggle profile picture based on the setting
+const showPfpNotice = 1; // Change this value to 0 to hide the intellectual property notice. Didn't make your pfp? be nice and say so
+// End of settings 
 function toggleProfilePicture() {
-    const profilPicture = document.querySelector('.profile-picture');
+    const profilePicture = document.querySelector('.profile-picture');
 
     if (showProfilePicture === 0) {
         profilePicture.classList.add('hidden');
@@ -15,4 +12,17 @@ function toggleProfilePicture() {
     }
 }
 
-document.addEventListener('DOMContentLoaded', toggleProfilePicture);
+function togglePfpNotice() {
+    const pfpNotice = document.querySelector('.pfp-notice');
+
+    if (showPfpNotice === 0) {
+        pfpNotice.classList.add('hidden');
+    } else {
+        pfpNotice.classList.remove('hidden');
+    }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    toggleProfilePicture();
+    togglePfpNotice();
+});
