0a8f18f1936530f60038c0a5619bc0d1e8dc55f0
hutlaw <154370020+Hutlaw@users.noreply.github.com>
2024-09-12 20:48:16 -0400

    Update scripts.js

--- cr/chests/scripts.js
+++ cr/chests/scripts.js
@@ -1,1 +1,13 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const editableCells = document.querySelectorAll('td[contenteditable="true"]');
 
+    editableCells.forEach(cell => {
+        cell.addEventListener('focus', () => {
+            cell.style.backgroundColor = '#777'; 
+        });
+
+        cell.addEventListener('blur', () => {
+            cell.style.backgroundColor = '';
+        });
+    });
+});
